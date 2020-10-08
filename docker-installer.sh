8312affc7111b00e69a9bf7dd8646b4dfc968b40
JJ Quispe <joelquispeunju@gmail.com>
2020-10-07 22:22:49 -0300

    test #4

--- docker-installer.sh
+++ docker-installer.sh
@@ -5,6 +5,9 @@ echo "************************"
 echo "DOCKER"
 echo "************************"
 
+echo "************************"
+echo "installing dependecies libraries"
+echo "************************"
 apt-get update
 apt-get install \
     apt-transport-https \
@@ -12,14 +15,26 @@ apt-get install \
     curl \
     gnupg-agent \
     software-properties-common
+
+echo "************************"
+echo "check keys"
+echo "************************"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
+
+echo "************************"
+echo "installing docker emgine"
+echo "************************"
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "************************"
+echo "docker engine testing"
+echo "************************"
 docker run hello-world
 
 echo "************************"
