85e9bfe3167ff46735ff3eeebf6f61a5b7b3b12d
JJ Quispe <joelquispeunju@gmail.com>
2020-10-07 22:13:35 -0300

    test #3

--- docker-installer.sh
+++ docker-installer.sh
@@ -1,30 +1,35 @@
-echo -e "Start installing...\n"
+echo "************************"
+echo "Start installing..."
+echo "************************"
+echo "************************"
 echo "DOCKER"
 echo "************************"
 
-sudo apt-get update
-sudo apt-get install \
+apt-get update
+apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
 docker run hello-world
 
-echo "\nDOCKER-COMPOSE\n"
 echo "************************"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "DOCKER-COMPOSE"
+echo "************************"
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 
-echo "\nFinished..."
+echo "************************"
+echo "Finished..."
 echo "************************"
